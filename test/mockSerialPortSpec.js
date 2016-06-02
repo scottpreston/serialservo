a53d7f674a0667b6f72907e6e485752459cc1cdd
Scott Preston <me@scottpreston.org>
2016-06-02 10:32:50 -0400

    added mock serial port dependency

--- test/mockSerialPortSpec.js
+++
@@ -1,8 +0,0 @@
-var mockSerialPort = require("../lib/mockSerialPort.js");
-
-describe("a mock serial servo controller should act like a serial port", function () {
-    var mock = new mockSerialPort();
-    it("a serial port should have a write method", function () {
-        expect(mock.write).toBeDefined();
-    });
-});
--- test/sscSpec.js
+++ test/sscSpec.js
@@ -1,15 +1,27 @@
 var SerialServoController = require("../lib/ssc.js");
 
 describe("serial servo controller with no options has default values", function () {
-    var ssc = new SerialServoController();
+
     it("ssc should have logging enabled by default", function () {
+        var ssc = new SerialServoController();
         expect(ssc.loggingEnabled).toBe(true);
+    });
+    it("ssc should have logging disabled by if passed into constructor", function () {
+        var ssc = new SerialServoController({loggingEnabled: false});
+        expect(ssc.loggingEnabled).toBe(false);
     });
 });
 
 describe("serial controller move functionality", function () {
-    var ssc = new SerialServoController();
-    it("should pass specific bytes to the serial port", function () {
+    it("should pass specific bytes to the serial port with logging enabled", function () {
+        var ssc = new SerialServoController();
+        spyOn(ssc.serialPort, 'write');
+        ssc.move(1, 2);
+        expect(ssc.serialPort.write).toHaveBeenCalledWith([255, 1, 2]);
+    });
+
+    it("should pass specific bytes to the serial port with logging disabled", function () {
+        var ssc = new SerialServoController({loggingEnabled: false});
         spyOn(ssc.serialPort, 'write');
         ssc.move(1, 2);
         expect(ssc.serialPort.write).toHaveBeenCalledWith([255, 1, 2]);
@@ -17,8 +29,9 @@ describe("serial controller move functionality", function () {
 });
 
 describe("serial controller nav functionality", function () {
-    var ssc = new SerialServoController();
+
     it("should pass specific bytes to the serial port", function () {
+        var ssc = new SerialServoController();
         spyOn(ssc, 'move');
         ssc.nav(1, 1);
         expect(ssc.move.calls.count()).toEqual(2);
@@ -26,8 +39,9 @@ describe("serial controller nav functionality", function () {
 });
 
 describe("serial controller timed move functionality", function () {
-    var ssc = new SerialServoController();
+
     it("call move twice", function () {
+        var ssc = new SerialServoController();
         jasmine.clock().install();
         spyOn(ssc, 'move');
         ssc.timedMove(1, 1, 50);
@@ -39,8 +53,9 @@ describe("serial controller timed move functionality", function () {
 });
 
 describe("serial controller timed navigation functionality", function () {
-    var ssc = new SerialServoController();
+
     it("should call move 4 times", function () {
+        var ssc = new SerialServoController();
         jasmine.clock().install();
         spyOn(ssc, 'move');
         ssc.timedNav(1, 1, 50);
